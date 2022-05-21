29192239d24b9ff30afd5dc71a893829e4b9c625
DmitryMix260 <dmitry-buyanov1995@yandex.ru>
2022-05-21 12:16:11 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <calcComponents />
+  </div>
+</template>
+
+<script>
+import calcComponents from './components/calcComponents.vue'
+
+export default {
+  name: 'App',
+  components: {
+    calcComponents
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
