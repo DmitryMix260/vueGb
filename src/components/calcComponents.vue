347153361387ede8416a5369e1227d75f1b0e0a8
DmitryMix260 <dmitry-buyanov1995@yandex.ru>
2022-06-08 20:45:23 +0300

    add pow, trunk, del

--- src/components/calcComponents.vue
+++ src/components/calcComponents.vue
@@ -1,37 +1,40 @@
 <template>
-    <div class="calc" id="calc">
-        <div class="main">
-            <input type="number" v-model.number="op1">
-            <input type="number" v-model.number="op2">
-            = {{ result }}
-        </div>
-        <div>
-          <button @click="sum">+</button>
-          <button @click="sub">-</button>
-          <button @click="mult">*</button>
-          <button @click="div">/</button>
-        </div>
-        <div>
-          <label for="checkbox">
-          <input type="checkbox" id="checkbox" v-model="checked"> Отобразить экранную клавиатуру
-          </label>
-        </div>
-        <div class="buttonNumber" v-show="checked">
-          <div><input type="radio" id="op1" value="op1" v-model="picked">
-            <label for="op1">Первый операнд</label>
-            <input type="radio" id="op2" value="op2" v-model="picked">
-            <label for="op2">Второй операнд</label>
-          </div>
-          <button v-for="number in numbers" :key="number" @click="addToInput(number)"> {{ number }}</button>
-        </div>
+  <div class="calc" id="calc">
+    <div class="main">
+      <input type="number" v-model.number="op1">
+      <input type="number" v-model.number="op2">
+      = {{ result }}
     </div>
+    <div>
+      <button @click="sum">+</button>
+      <button @click="sub">-</button>
+      <button @click="mult">*</button>
+      <button @click="div">/</button>
+      <button @click="pow">^n</button>
+      <button @click="trunc">trunc</button>
+    </div>
+    <div>
+      <label for="checkbox">
+        <input type="checkbox" id="checkbox" v-model="checked"> Отобразить экранную клавиатуру
+      </label>
+    </div>
+    <div class="buttonNumber" v-show="checked">
+      <div><input type="radio" id="op1" value="op1" v-model="picked">
+        <label for="op1">Первый операнд</label>
+        <input type="radio" id="op2" value="op2" v-model="picked">
+        <label for="op2">Второй операнд</label>
+      </div>
+      <button v-for="number in numbers" :key="number" @click="addToInput(number)"> {{ number }}</button>
+      <button @click="delFromInput">Del</button>
+    </div>
+  </div>
 </template>
 
 <script>
 
 export default {
   name: 'calcComponents',
-  data () {
+  data() {
     return {
       op1: 0,
       op2: 0,
@@ -42,20 +45,29 @@ export default {
     }
   },
   methods: {
-    sum () {
+    sum() {
       this.result = this.op1 + this.op2
     },
-    sub () {
+    sub() {
       this.result = this.op1 - this.op2
     },
-    mult () {
+    mult() {
       this.result = this.op1 * this.op2
     },
-    div () {
+    div() {
       this.result = this.op1 / this.op2
     },
-    addToInput (number) {
+    pow() {
+      this.result = Math.pow(this.op1, this.op2)
+    },
+    trunc() {
+      this.result = Math.trunc(this.op1 / this.op2)
+    },
+    addToInput(number) {
       this[this.picked] = this[this.picked] * 10 + number
+    },
+    delFromInput() {
+      this[this.picked] = Math.trunc(this[this.picked] / 10)
     }
   }
 }
@@ -64,5 +76,8 @@ export default {
 <style lang="scss">
 #calc :nth-last-child(n) {
   margin-bottom: 10px;
+}
+button {
+  margin-left: 5px;
 }
 </style>
