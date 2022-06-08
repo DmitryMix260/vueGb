58ffbe2532dd2cdcadf4a63fd3649d75c638938d
DmitryMix260 <dmitry-buyanov1995@yandex.ru>
2022-06-08 20:28:37 +0300

    comtpleted lesson1

--- src/components/calcComponents.vue
+++ src/components/calcComponents.vue
@@ -1,24 +1,26 @@
 <template>
-    <div class="calc">
-        <div class="main">
-            <input type="number" v-model.number="op1">
-            <input type="number" v-model.number="op2">
-            = {{ result }}
-        </div>
-        <div>
-          <button @click="sum">+</button>
-          <button @click="sub">-</button>
-          <button @click="mult">*</button>
-          <button @click="div">/</button>
-        </div>
+  <div class="calc">
+    <div class="main">
+      <input type="number" v-model.number="op1">
+      <input type="number" v-model.number="op2">
+      = {{ result }}
     </div>
+    <div>
+      <button @click="sum">+</button>
+      <button @click="sub">-</button>
+      <button @click="mult">*</button>
+      <button @click="div">/</button>
+      <button @click="pow">^n</button>
+      <button @click="trunc">trunc</button>
+    </div>
+  </div>
 </template>
 
 <script>
 
 export default {
   name: 'calcComponents',
-  data () {
+  data() {
     return {
       op1: 0,
       op2: 0,
@@ -26,22 +28,30 @@ export default {
     }
   },
   methods: {
-    sum () {
+    sum() {
       this.result = this.op1 + this.op2
     },
-    sub () {
+    sub() {
       this.result = this.op1 - this.op2
     },
-    mult () {
+    mult() {
       this.result = this.op1 * this.op2
     },
-    div () {
+    div() {
       this.result = this.op1 / this.op2
+    },
+    pow() {
+      this.result = Math.pow(this.op1, this.op2)
+    },
+    trunc() {
+      this.result = Math.trunc(this.op1 / this.op2)
     }
   }
 }
 </script>
 
 <style scoped lang="scss">
-
+.calc {
+  background-color: aquamarine;
+}
 </style>
